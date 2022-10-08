e4b148e535152d8032a3656231db719f927c7199
Raisess <danilosantana456789@gmail.com>
2022-10-08 18:07:14 -0300

    upgrade to a new version of blob

--- src/generator.py
+++ src/generator.py
@@ -3,22 +3,19 @@ from env import Env
 from view import ListView, PostView
 
 class Generator:
-  def __init__(self, post: str):
-    self._post = post
-
-  def generate(self):
+  def generate():
     env = Env()
+    if not os.path.isdir("./blog"):
+      os.mkdir("./blog")
 
-    if self._post == "index":
-      view = ListView(env.get("STATIC_TITLE"))
-      file = open("index.html", "w")
-      file.write(view.content)
-      file.close()
-    else:
-      if not os.path.isdir("./posts"):
-        os.mkdir("./posts")
+    view = ListView(env.get("STATIC_TITLE"))
+    file = open("./blog/index.html", "w")
+    file.write(view.content)
+    file.close()
 
-      path = f"./posts/{self._post}.html"
+    posts = os.listdir("./inputs")
+    for post in posts:
+      path = f"./blog/{post}"
       view = PostView(env.get("STATIC_TITLE"), path)
       file = open(path, "w")
       file.write(view.content)
